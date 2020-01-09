7371bfa554a52def1a8a211a800c76bcdbd47ea3
homesangsang <homesangsang@qq.com>
2020-01-09 10:45:32 +0800

    fea: struct的tag示例

---
+++ struct/struct_tag_demo.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
+
+type StructDemo struct {
+	Id        string `json:"id"`
+	FirstName string `json:"firstName" bson:"first_name"`
+	LastName  string `json:"lastName" bson:"last_name"`
+}
+
+func structDemo() {
+	demo := StructDemo{Id: "202001011010", FirstName: "Smith", LastName: "John"}
+	j, _ := json.Marshal(demo)
+	fmt.Println(string(j))
+}
+
+func anotherDemo() {
+	type User struct {
+		UserId   int    `json:"userId" bson:"user_id"`
+		UserName string `json:"userName" bson:"user_name"`
+	}
+	// 输出json格式
+	u := &User{UserId: 1, UserName: "tony"}
+	j, _ := json.Marshal(u)
+	fmt.Println(string(j))
+	// 输出内容：{"user_id":1,"user_name":"tony"}
+
+	// 获取tag中的内容
+	t := reflect.TypeOf(u)
+	field := t.Elem().Field(0)
+	fmt.Println(field.Tag.Get("json"))
+	// 输出：user_id
+	fmt.Println(field.Tag.Get("bson"))
+	// 输出：user_id
+}
+
+func main() {
+	structDemo()
+	fmt.Println()
+	anotherDemo()
+}
